6f669d7696c8650d122672d9ca483181ee465493
S Tukra <35964759+SamPlvs@users.noreply.github.com>
2018-04-03 23:38:55 +0100

    Add files via upload

---
+++ visualise_HOG.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 20:53:18 2017
+
+@author: Samyakh Tukra
+"""
+#%%
+import matplotlib.pyplot as plt
+from skimage import io
+from skimage.feature import hog
+from skimage import data, color, exposure
+from PIL import Image
+#%%
+img = io.imread(r"E:\Documents\Cardiff University (MedEng)\Year 4 Study\Mechatronics Design Project\SVM_dataset_latest\pos_images_mine\83.jpg")
+#im= Image.open(r"E:\Documents\Cardiff University (MedEng)\Year 4 Study\Mechatronics Design Project\SVM_dataset_latest\pos_images_mine\1.jpg")
+image = color.rgb2gray(img)
+
+fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualise=True)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+ax1.axis('off')
+ax1.imshow(image, cmap=plt.cm.gray)
+ax1.set_title('Input image')
+ax1.set_adjustable('box-forced')
+
+# Rescale histogram for better display
+hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+
+ax2.axis('off')
+ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+ax2.set_title('Histogram of Oriented Gradients')
+ax1.set_adjustable('box-forced')
+plt.show()
